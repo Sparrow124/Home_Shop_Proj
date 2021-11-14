3ae2f1b32ae5552fd5a95a6c9ed23d206b624797
Sparrow <dronnikov_225@mail.ru>
2021-11-15 00:46:32 +0700

    Created base html and some daughter templates, made View for main page and Mixin

--- products/urls.py
+++ products/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from .views import *
 
 urlpatterns = [
-     path('', ),
+     path('', MainPageContent.as_view(), name='lobby_page_url'),
+     path('product/<str:slug>/', ProductDetail.as_view(), name='product_detail_url'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import *
+from django.views.generic import View
+from .utils import *
 
-# Create your views here.
+
+class MainPageContent(View):
+    def get(self, request):
+        products = Product.objects.all()
+        return render(request, 'products/main_page.html', context={'products': products})
+
+
+class ProductDetail(ObjectDetailMixin, View):
+    model = Product
+    template = 'products/prod_info.html'
